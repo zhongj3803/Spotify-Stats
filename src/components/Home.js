fc16f16245743ef933d71eb2d5f1b407c5ee223d
Jesse Zhong <jessezhong@gmail.com>
2023-07-12 23:02:31 -0700

    :D

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div>
-            <h1 className="description">This is Spotify Stats! Learn more about your own listening choices.</h1>
+            <h1 className="description">This is Spotify Stats! Learn more about your own listening choices!!</h1>
         </div>
     );
   }
